7a90e11f357b12d236d118780792a2d1c041e7e4
SHERRY <swasieko@gmail.com>
2018-07-05 17:35:06 +0000

    User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :username, presence: true, length: { maximum: 25 }
+  validates :email, presence: true, uniqueness: {case_sensitive: false },
+              length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }
+  
+  
 end
