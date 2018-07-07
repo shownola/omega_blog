9fe56f7b22f04be59dff693eb1f56fe475ce2d0f
SHERRY <swasieko@gmail.com>
2018-07-07 20:17:42 +0000

    Fix current user to create articles, sort, added icons to index view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,8 @@ class ArticlesController < ApplicationController
   before_action :require_same_user, only: [:edit, :update, :destroy]
   
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 10)
+    @articles = Article.paginate(page: params[:page], per_page: 10).order(created_at: :desc)
+    
   end
   
   def new
@@ -13,7 +14,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.user = User.last
+    @article.user = current_user
     if @article.save
       flash[:success] = 'Article has been created'
       redirect_to @article
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
-  
   validates :title, presence: true, length: { minimum: 2, maximum: 150 }
   validates :description, presence: true, length: { minimum: 2, maximum: 1000 }
   validates :user_id, presence: true
+ 
   
 end
