4f1c28dacb0dcfa6c88ab9d86a2cefd240b249e6
SHERRY <swasieko@gmail.com>
2018-07-07 20:57:06 +0000

    Add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Omega Blog, #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       flash[:danger] = "There was an error, please try again"
       render 'new'
