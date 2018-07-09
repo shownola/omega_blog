0813250657713af4dddb857995c290fafa39bd85
SHERRY <swasieko@gmail.com>
2018-07-09 03:54:47 +0000

    Update all category views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,6 +21,7 @@ class CategoriesController < ApplicationController
   
   def show
     @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 10)
   end
   
   
--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -2,6 +2,5 @@ class ArticleCategory < ApplicationRecord
   belongs_to :article
   belongs_to :category
   
-  validates :article_id, presence: true, numericality: true
-  validates :category_id, presence: true, numericality: true
+  
 end
