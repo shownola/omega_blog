7768a222510d5e3de4486467a5c2bb40f424997c
SHERRY <swasieko@gmail.com>
2018-07-09 04:29:42 +0000

    Complete category actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,9 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show]
+  before_action :find_category, only: [:edit, :show, :update, :destroy]
   
   def index
-    @categories = Category.paginate(page: params[:page], per_page: 10)
+    @categories = Category.paginate(page: params[:page], per_page: 10).order('name ASC')
   end
   
   def new
@@ -19,13 +20,35 @@ class CategoriesController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @category.update(category_params)
+      flash[:success] = 'Category has been updated'
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+  
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 10)
   end
   
+  def destroy
+    @category.destroy
+    flash[:danger] = "Your category was deleted"
+    redirect_to categories_path
+  end
+  
   
   private
+  
+    def find_category
+      @category = Category.find(params[:id])
+    end
   
     def category_params
       params.require(:category).permit(:name)
